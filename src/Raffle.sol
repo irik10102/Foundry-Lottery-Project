e00286c0293475a673c6e4aa34699cc80b365c9e
irik10102 <sahilghosh10102@gmail.com>
2026-04-29 19:54:14 +0530

    Added Event

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -2,18 +2,22 @@
 
 pragma solidity ^0.8.0;
 
+/*Errors*/
+error NotEnoughETHToEnterRaffle();
+
 /**
  * @title Raffle Smart Contract
  * @author Sahil Ghosh
  * @notice A simple raffle creation
  * @dev It implements Chainlink VRFv2.5
- *
  */
-contract Raffle {
-    /*Errors*/
-    error NotEnoughETHToEnterRaffle();
 
+contract Raffle {
     uint256 private immutable i_entranceFees;
+    address[] private s_players;
+
+    /*Events*/
+    event EnteredRaffle(address indexed player);
 
     constructor(uint256 ent_fees) {
         i_entranceFees = ent_fees;
@@ -23,6 +27,8 @@ contract Raffle {
         if (msg.value < i_entranceFees) {
             revert NotEnoughETHToEnterRaffle();
         }
+        s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender);
     }
 
     function pickWinner() public returns (address) {}
