6cea3b68ac4dd6571255a2d2b09ba50653163b6a
irik10102 <sahilghosh10102@gmail.com>
2026-04-29 19:09:15 +0530

    Added Revert

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -5,16 +5,31 @@ pragma solidity ^0.8.0;
 /**
  * @title Raffle Smart Contract
  * @author Sahil Ghosh
+ * @notice A simple raffle creation
+ * @dev It implements Chainlink VRFv2.5
  *
  */
 contract Raffle {
-    uint256 private immutable i_entrance_fees;
+    /*Errors*/
+    error NotEnoughETHToEnterRaffle();
+
+    uint256 private immutable i_entranceFees;
 
     constructor(uint256 ent_fees) {
-        i_entrance_fees = ent_fees;
+        i_entranceFees = ent_fees;
     }
 
-    function enterRaffle() external payable {}
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFees) {
+            revert NotEnoughETHToEnterRaffle();
+        }
+    }
 
     function pickWinner() public returns (address) {}
+
+    /* Getter Functions*/
+
+    function getEntraceFees() external view returns (uint256) {
+        return i_entranceFees;
+    }
 }
